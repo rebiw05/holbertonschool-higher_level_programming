46ef62e95d1796a893d545023370d0816ae5f4c9
root <ismayilovarabia5@gmail.com>
2025-06-14 16:57:47 +0000

    power

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return a ^ b
+    result = 1
+    if b == 0:
+        return 1
+    elif b > 0:
+        for _ in range(b):
+            result *= a
+    else:
+        for _ in range(-b):
+            result *= a
+        result = 1 / result
+    return result
