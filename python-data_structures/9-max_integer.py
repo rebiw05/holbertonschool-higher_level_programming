86da39cd1382381f3b6a7006123a78083215e9c8
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-19 11:04:19 +0000

    max value iin the list

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max_value = my_list[0]
+    if len(my_list) == 0:
+        return None
+    else:
+        for i in my_list:
+            if i > max_value:
+                max_value = i
+                return i
