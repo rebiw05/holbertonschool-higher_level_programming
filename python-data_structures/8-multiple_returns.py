c1f17ec4faf6e07a3f4f3f44cc9a397ffb5c57a6
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-19 10:53:07 +0000

    multiple returns first and legth

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) <= 0:
-        return None
-    else:
-        return len(sentence), sentence[0]
+    length = len(sentence)
+    first_char = sentence[0] if length > 0 else None
+    return (length, first_char)
