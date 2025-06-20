7dee875d98fb49ea9ee8522baaa4ac94bc9c4b86
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-20 12:27:33 +0000

    different elements

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -4,4 +4,7 @@ def only_diff_elements(set_1, set_2):
     for i in set_1:
         if i not in set_2:
             differs.append(i)
+    for j in set_2:
+        if j not in set_1:
+            differs.append(j)
     return differs
