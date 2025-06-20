5760df40f4de5bef6810ce886ba39abebe035b91
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-20 09:05:55 +0000

    delete item from the list

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0:
+        return my_list
+    else:
+        for i in my_list:
+            if i == my_list[idx]:
+                my_list.remove(i)
+        return my_list
