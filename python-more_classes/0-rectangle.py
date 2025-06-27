3692ab5f39cc8b142a5993041a77cc0a6db9178f
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-27 18:00:32 +0000

    defining rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""
+this module ilustrates the rectangle definening
+
+with class and pass module
+"""
+
+
 class Rectangle:
+    """
+    defining rectangle with class.
+    pass method help to ignore the class
+    """
     pass
