dc6494c080c13b47d48fdf86ed1aa6a5f9090c3a
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-27 19:19:41 +0000

    rectangle width height

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
 
         @property
         def width(self):
-            return self.width
+            return self.__width
 
         @width.setter
         def width(self, value):
@@ -37,11 +37,11 @@ class Rectangle:
                 raise TypeError("width must be an integer")
             if value < 0:
                 raise ValueError("width must be >= 0")
-            self.width = value
+            self.__width = value
 
         @property
         def height(self):
-            return self.height
+            return self.__height
 
         @height.setter
         def height(self, value):
@@ -49,4 +49,4 @@ class Rectangle:
                 raise TypeError("height must be an integer")
             if value < 0:
                 raise ValueError("height must be >= 0")
-            self.height = value
+            self.__height = value
