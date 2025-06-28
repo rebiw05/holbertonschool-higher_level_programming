5d2b1edd3712ef03193c8e90d84ae0929d4b1b39
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-28 14:18:54 +0000

    is kind of class and ist subs

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """
+This module ilustrates obj that exist
+in specifc class and its subs
 """
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    is kin of class function
+    """
     if not isinstance(obj, a_class):
         return False
     return True
