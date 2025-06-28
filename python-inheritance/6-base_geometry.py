095121170925584873b800cd74bcbe56d81933c4
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-28 14:41:02 +0000

    area not implemented

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+this defines basegeomerty class with
+public instances method
+"""
+
+
+class BaseGeometry:
+    """
+    this is an empty class
+    """
+
+    def area(self):
+        """
+        this raises an error
+        """
+        raise Exception("area() is not implemented")
