ceeffd99363e5e3659b9f4480790ac106d7932f5
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-28 14:30:55 +0000

    inherits objects

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,4 +5,7 @@ this module defines subclass obj
 
 
 def inherits_from(obj, a_class):
+    """
+    this function illustrates a_class subs objects
+    """
     return isinstance(obj, a_class) and type(obj) is not a_class
