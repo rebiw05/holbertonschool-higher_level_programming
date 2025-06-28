8331ac4ee083cb34770a056f62ea1ee3396c2a16
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-28 17:01:43 +0000

    10 rectang

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module defines the Rectangle class, which inherits from BaseGeometry.
+This module defines the Square class, which inherits from Rectangle.
 """
 
 
@@ -9,26 +9,23 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """
-    A class representing a rectangle, inheriting from BaseGeometry.
+    A class representing a square, inheriting from Rectangle.
 
-    Attributes:
-        __width (int): The width of the rectangle (private).
-        __height (int): The height of the rectangle (private).
+    A square is a special type of rectangle where all sides are equal.
+
     """
 
     def __init__(self, size):
         """
-        Initializes a new Rectangle instance.
+        Initializes a new Square instance.
 
         Args:
-            size (int): The width of the rectangle. Must be a positive.
+            size (int): length of the square. Must be a positive integer.
 
         Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is less than or equal to 0.
-        """
-        self.integer_validator("size", width)
-
+            TypeError: If size is not an integer.
+            """
+        self.integer_validator("size", size)
         super().__init__(size, size)
-        # Assign the validated values to private instance attributes
+
         self.__size = size
