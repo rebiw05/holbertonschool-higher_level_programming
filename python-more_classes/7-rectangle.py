a493bdda2949d8a1cdb9c4a568e4b006f023d16d
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-28 09:32:30 +0000

    str format print symbol

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -122,7 +122,8 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join([str(self.print_symbol) * self.__width for _ in range(self.__height)])
+        sstr = str(self.print_symbol)
+        return "\n".join([sstr * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
