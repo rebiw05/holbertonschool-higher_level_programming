9acc8b6c56116f9f0d64e4c977a0665c6fadb359
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-28 16:50:04 +0000

    rectangle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -2,6 +2,8 @@
 """
 This module defines the Rectangle class, which inherits from BaseGeometry.
 """
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
@@ -20,7 +22,7 @@ class Rectangle(BaseGeometry):
 
         Args:
             width (int): The width of the rectangle. Must be a positive.
-            height (int): The height of the rectangle. Must be a positivee.
+            height (int): The height of the rectangle. Must be a positive.
 
         Raises:
             TypeError: If width or height is not an integer.
@@ -28,14 +30,37 @@ class Rectangle(BaseGeometry):
         """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+
+        # Assign the validated values to private instance attributes
         self.__width = width
         self.__height = height
 
-        def area():
-            """
-            this is function
-            """
-            return self.__width * self.__height
+    def area(self):
+        """
+        Calculates and returns the area of the rectangle.
 
-        def __str__(self):
-            return f"[Rectangle] {self.__width}/{self.__height}"
+        Returns:
+            int: The area of the rectangle (width * height).
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Returns a string representation of the Rectangle instance.
+        The format is: [Rectangle] <width>/<height>.
+
+        Returns:
+            str: The string representation of the rectangle.
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    # __repr__ is often implemented for debugging, and by default,
+    # if __str__ is defined but __repr__ is not, print() uses __str__.
+    # However, explicitly defining __repr__ to call __str__ is good practice
+    # if you want the same representation for both.
+    def __repr__(self):
+        """
+        Returns a string representation of the Rectangle instance,
+        useful for debugging. Calls __str__.
+        """
+        return self.__str__()
