8677f9b1a50d020cc75ee4b2494f24731bee07fd
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-28 12:24:56 +0000

    listing obj of the functiton

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect. This can be an instance of a class,
+             a class itself, or any other Python object (e.g., int, str).
+
+    Returns:
+        A list of strings, where each string is the name of an attribute
+        or method available for the given object. The list is typically
+        sorted alphabetically.
+"""
+
+
+def lookup(obj):
+    return dir(obj)
