f38363e04b8ff2b3e5d20a2792633c748adb0eb5
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-28 15:06:28 +0000

    validate the value

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+this defines empty class and functions
+"""
+
+
+class BaseGeometry:
+    """
+    this is empty class
+    """
+
+    def area(self):
+        """
+        this raise an error
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        this validates the value
+        """
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
+        self.__name = value
