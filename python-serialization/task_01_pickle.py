3fd36bf47a44a0e8ed9c2e8cd65025009a18d778
rebiw05 <ismayilovarabia5@gmail.com>
2025-06-30 10:46:30 +0000

    serializing with pickle

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,6 +1,12 @@
-import pickle # Import the pickle module for serialization
+#!/usr/bin/python3
+"""
+this is module
+"""
 
-# Define the CustomObject class
+
+import pickle
+
+
 class CustomObject:
     """
     A custom class to represent an object with a name, age, and student status.
@@ -28,14 +34,18 @@ class CustomObject:
         """
         Displays the attributes of the CustomObject instance.
         """
-        print(f"Name: {self.name}\nAge: {self.age}\nStudent: {self.is_student}")
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename: str):
         """
-        Serializes the current instance of CustomObject to a file using the pickle module.
+        Serializes the current instance of CustomObject
+        to a file using the pickle module.
 
         Args:
-            filename (str): The name of the file to save the serialized object to.
+            filename (str): The name of the file
+            saving the serialized object to.
         """
         with open(filename, "wb") as file:
             pickle.dump(self, file)
@@ -43,11 +53,14 @@ class CustomObject:
     @classmethod
     def deserialize(cls, filename: str):
         """
-        Deserializes a CustomObject instance from a file using the pickle module.
-        This is a class method, so it can be called directly on the class (e.g., CustomObject.deserialize()).
+        Deserializes a CustomObject instance from
+        file using the pickle module.
+        This is a class method,
+        so it can be called directly on the class.
 
         Args:
-            filename (str): The name of the file to load the serialized object from.
+            filename (str): The name of the file
+            to load the serialized object from.
         """
         with open(filename, "rb") as file:
             return pickle.load(file)
