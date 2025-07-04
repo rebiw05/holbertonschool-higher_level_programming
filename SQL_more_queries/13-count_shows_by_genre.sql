1260850f7af4071f6a6e508d27f461cab003cf30
rebiw05 <ismayilovarabia5@gmail.com>
2025-07-04 23:43:58 +0000

    using count and group by

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,10 @@
+--  lists all genres
+SELECT
+    tv_genres.name as genre,
+    COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM
+    tv_show_genres
+LEFT JOIN
+    tv_genres ON tv_genres.id=tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
