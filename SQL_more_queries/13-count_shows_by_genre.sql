4ceb725de93e5315eac9b5fb47d8ee75ab08cf68
rebiw05 <ismayilovarabia5@gmail.com>
2025-07-04 23:45:24 +0000

    using inner join

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,7 +4,7 @@ SELECT
     COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM
     tv_show_genres
-LEFT JOIN
+INNER JOIN
     tv_genres ON tv_genres.id=tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
