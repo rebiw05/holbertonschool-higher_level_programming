6b3240afd40e91f9bd7de13a5e5ff864c0d6d2cc
rebiw05 <ismayilovarabia5@gmail.com>
2025-07-05 13:42:51 +0000

    using sum

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- compute ratings
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+SELECT tv_shows.title AS title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 LEFT JOIN tv_show_ratings ON tv_shows.id=tv_show_ratings.show_id
 GROUP BY tv_shows.title
