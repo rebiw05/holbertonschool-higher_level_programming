7080b6270ecb01feb00d94fdc99223bbc1cbec3e
rebiw05 <ismayilovarabia5@gmail.com>
2025-07-11 21:16:37 +0000

    url type content decoding

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
+"""
+this is module
+"""
 
+
+import urllib.request
+"""
+this is for using url
+"""
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    headers = {
+        'cfclearance': 'true'
+    }
+    req = urllib.request.Request(url, headers=headers)
+
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
