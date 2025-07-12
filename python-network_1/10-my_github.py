5b6a17748ef51c387f1b943efcc399497158229c
rebiw05 <ismayilovarabia5@gmail.com>
2025-07-12 01:51:46 +0000

    my github credentials

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""
-this is module
-"""
-
+"""Uses GitHub API to display the authenticated user's ID using Basic Auth"""
 
 import sys
 import requests
-"""
-this is for using url
-"""
 
 if __name__ == "__main__":
     username = sys.argv[1]
     token = sys.argv[2]
-    url = "https://github.com/rebiw05"
+    url = "https://api.github.com/user"  # ✅ correct endpoint
+
     response = requests.get(url, auth=(username, token))
+
     if response.status_code == 200:
         user_data = response.json()
         print(user_data.get("id"))
