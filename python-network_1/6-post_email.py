3c86e794596fe4e5b9743fef36b14cb534b99702
rebiw05 <ismayilovarabia5@gmail.com>
2025-07-12 01:27:28 +0000

    post email with requests

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -13,6 +13,6 @@ this is for using url
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = {"email" : email}
-    response = requests.post(url, data)
+    data = {'email': email}
+    response = requests.post(url, data=data)
     print("Your email is: {}".format(response.text))
