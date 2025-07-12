ab2e95ec9cf529108c98b68bf2475b0e815914c2
rebiw05 <ismayilovarabia5@gmail.com>
2025-07-12 00:41:45 +0000

    using requests

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -13,7 +13,7 @@ this is for fetching
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     headers = {"cfclearance": "true"}
-    response = requests.get(url)
+    response = requests.get(url, headers=headers)
     content = response.text
     print("Body response:")
     print("\t- type: {}".format(type(content)))
