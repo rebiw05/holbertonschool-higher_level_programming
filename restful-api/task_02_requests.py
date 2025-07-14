e802e472e63203a0ab8b6ad88ec38974df69d0be
rebiw05 <ismayilovarabia5@gmail.com>
2025-07-14 07:43:27 +0000

    handling api using python

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -19,28 +19,42 @@ def fetch_and_print_posts():
     """
     this is function
     """
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        posts = response.json()
-        for i, post in posts:
-            print(f"post title: {post[title]}")
-    else:
-        print(response.status_code)
+    try:
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            posts = response.json()
+            for i, post in posts:
+                print(f"post title: {post[title]}")
+        else:
+            print(response.status_code)
+    except requests.exceptions.RequestException as e:
+        print(f"fault: {e}")
+    except json.JSONDecodeError:
+        print("fail")
 
 
 def fetch_and_save_posts():
     """
     this is function
     """
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        csv_file_name = "posts_data.csv"
-        with open(csv_file_name, 'w', newline='', encoding='utf-8') as file:
-            fieldnames = posts[0].keys() if posts else []
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
-            for post in posts:
-                writer.writerow(post)
-        print(f"{csv_file_name}")
-    else:
-        print(response.status_code)
+    try:
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            posts = response.json
+            csv_file_name = "posts.csv"
+            fieldnames = ['id', 'title', 'body', 'userId']
+            with open(csv_file_name, 'w', newline='', encoding='utf-8') as csvfile:
+                fieldnames = posts[0].keys() if posts else []
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                writer.writeheader()
+                for post in posts:
+                    writer.writerow(post)
+            print(f"{csv_file_name}")
+        else:
+            print(response.status_code)
+    except requests.exceptions.RequestException as e:
+        print(f"{e}")
+    except IOError as e:
+        print(f"{e}")
+    except json.JSONDecodeError:
+        print("error")
