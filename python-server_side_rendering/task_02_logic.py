c1ada58e30ff78524c36bd24ae83c7f301a86a3a
rebiw05 <ismayilovarabia5@gmail.com>
2025-07-18 21:40:53 +0000

    if for stataement

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,29 @@
+uufrom flask import Flask, render_template
+import json
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    return render_template('items.html')
+
+file_name = f"items.json"
+        try:
+            with open(file_name, 'r') as f:
+                content = json.load(f)
+                print(content)
+        except Exception as e:
+            print(f"Failed to write {file_name}: {e}")
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
