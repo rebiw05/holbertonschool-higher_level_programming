eed9719570f646173f65a912790a25a79b6082a8
rebiw05 <ismayilovarabia5@gmail.com>
2025-07-23 00:18:27 +0000

    print ascii reverse order and alternating upper lower

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print("".join(chr(i+32 if i % 2 else i) for i in range(90, 64, -1)), end="")
+print("{}".format("".join(
+    chr(i + 32) if i % 2 else chr(i)
+    for i in range(90, 64, -1)
+)), end="")
